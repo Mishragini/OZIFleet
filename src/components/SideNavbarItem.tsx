d1f2de63d0a858201fe767bd20334edc0c28680b
Mishragini <mishragini11.com>
2024-04-12 22:13:49 +0530

    modifies SideNavbarItem

--- src/components/SideNavbarItem.tsx
+++ src/components/SideNavbarItem.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export const SideNavbarItem=({title,icon}:{title:string,icon:React.ReactNode})=>{
     return(
-        <div className="flex bg-white text-normal font-medium p-4">
+        <div className="flex bg-white text-normal font-medium p-4 items-center">
             <div className="mr-2">{icon}</div>
             <div>{title}</div>
         </div>
